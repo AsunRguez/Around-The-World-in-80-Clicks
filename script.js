6247ac99189c2e5b1e20cf80b4d912762bef7d25
Marco <marco.izquierdo.ochovo2@gmail.com>
2024-02-19 15:52:58 +0100

    FINAL script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("search");
+    const images = document.querySelectorAll(".image-container img");
+
+    searchInput.addEventListener("keyup", function() {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        images.forEach(function(image) {
+            const altText = image.alt.toLowerCase();
+
+            if (altText.includes(searchTerm)) {
+                image.style.display = "inline";
+            } else {
+                image.style.display = "none";
+            }
+        });
+    });
+});
